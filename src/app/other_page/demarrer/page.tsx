fec0ad11f293e98dcde8b681cea98aec92add45e
planetechsarl <planetechsarlbenin@gmail.com>
2025-01-29 19:04:54 +0100

    berno_demarrer

--- src/app/other_page/demarrer/page.tsx
+++ src/app/other_page/demarrer/page.tsx
@@ -10,16 +10,15 @@ const DEMARRER = () => {
         style={{ backgroundImage: `url(${image})` }}
       >
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
-          <div className="text-center text-white">
-            <h1 className="text-5xl font-bold mb-4">Débloquer mon Icloud</h1>
-            <p className="text-xl mb-8">Explore the best content on the web</p>
+          <div className="text-center text-white px-4 md:px-8">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">Débloquer mon Icloud</h1>
+            <p className="text-lg sm:text-xl md:text-2xl mb-8">Explore the best content on the web</p>
             <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300">
               Déblocage rapide
             </button>
           </div>
         </div>
       </div>
-     
     </div>
   );
 };
