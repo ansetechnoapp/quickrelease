5c4dc0c860cdbeb3139c79b46a6563e0e54b70b5
otty kevin <anselmetechnoapp@gmail.com>
2025-03-26 01:30:04 +0100

    feat: add openGraph metadata for improved SEO and update HTML language attribute to French

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,17 @@ export const metadata: Metadata = {
   title: "Deblocage rapide de vos iPhone",
   description: "Libérez rapidement tout le potentiel de votre iPhone grâce à notre service de déblocage efficace et fiable. Que ce soit pour utiliser votre appareil avec n'importe quel opérateur ou simplement pour profiter d'une liberté totale, nous vous offrons une solution simple et rapide. Fini les tracas liés au blocage réseau – avec nous, votre iPhone est prêt à fonctionner où vous voulez, quand vous voulez !",
   icons: {
-    icon: '/icon.png',
+    icon: '/icon.png', 
     // Uncomment these lines if you have the corresponding favicon files
     // apple: '/apple-touch-icon.png',
     // shortcut: '/favicon.ico',
   },
+  openGraph: {
+    title: 'Deblocage rapide de vos iPhone',
+    description: "Libérez rapidement tout le potentiel de votre iPhone grâce à notre service de déblocage efficace et fiable. Que ce soit pour utiliser votre appareil avec n'importe quel opérateur ou simplement pour profiter d'une liberté totale, nous vous offrons une solution simple et rapide. Fini les tracas liés au blocage réseau – avec nous, votre iPhone est prêt à fonctionner où vous voulez, quand vous voulez !",
+    type: 'website',
+    locale: 'fr_FR',
+  }
 };
 
 export default function RootLayout({
@@ -31,7 +37,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Uncomment these lines if you have the corresponding favicon files */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <GoTopButton />
